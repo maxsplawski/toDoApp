bdca89be9a7ec39894e971b3ecd7eeb926024a07
Max <mniamek9@gmail.com>
2022-02-17 16:53:21 +0100

    Development

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const form = document.getElementById("todo-form");
-const toDoInput = document.getElementById("todo");
-const submitBtn = document.querySelector(".btn-submit");
+const form = document.querySelector(".form");
+const toDoInput = document.querySelector(".form__input");
+const toDoBtn = document.querySelector(".btn-submit");
+const toDoList = document.querySelector(".todo__list");
+
+let toDosArr = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e);
+  const data = new FormData(form);
+  data.forEach((toDo) => {
+    toDosArr.push(toDo);
+  });
+  console.log(toDosArr);
 });
+
+const renderToDos = (toDos = ["fesfs"]) => {
+  toDoList.innerHTML = "";
+
+  toDos.forEach((toDo) => {
+    const html = `<div>${toDo}</div>`;
+
+    toDoList.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+renderToDos();
