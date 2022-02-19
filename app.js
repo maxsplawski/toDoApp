2c60920089832496378f71a6bb5bd346828c4c03
Max <mniamek9@gmail.com>
2022-02-19 16:36:26 +0100

    Development

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const form = document.querySelector(".form");
-const taskInput = document.querySelector(".form__input");
+const taskInput = document.querySelector(".form__input-task");
+const dateInput = document.querySelector("form__input-date");
+const cancelBtn = document.querySelector(".btn-reset");
 const taskList = document.querySelector(".task__list");
 let taskArr = [];
 
@@ -7,50 +9,84 @@ const renderTask = (e) => {
   e.preventDefault();
   const task = taskInput.value;
   const convertedTask = task[0].toUpperCase() + task.slice(1);
+
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = time.getMonth();
+  const day = time.getDate();
+
   taskArr.push(convertedTask);
   console.log(convertedTask);
 
-  taskElContainer = document.createElement("div");
-  taskElContainer.classList.add("task");
+  taskContainerEl = document.createElement("div");
+  taskContainerEl.classList.add("task");
 
-  taskEl = document.createElement("div");
-  taskEl.classList.add("task__content");
-  taskEl.textContent = convertedTask;
+  taskContentEl = document.createElement("div");
+  taskContentEl.classList.add("task__content");
+
+  taskDateElContainer = document.createElement("div");
+  taskDateElContainer.classList.add("task__date-container");
+
+  taksDateYearEl = document.createElement("p");
+  taksDateYearEl.classList.add("task_date-year");
+  taksDateYearEl.textContent = year;
+
+  taksDateMonthEl = document.createElement("p");
+  taksDateMonthEl.classList.add("task_date-month");
+  taksDateMonthEl.textContent = month;
+
+  taksDateDayEl = document.createElement("p");
+  taksDateDayEl.classList.add("task_date-day");
+  taksDateDayEl.textContent = day;
+
+  taskEl = document.createElement("input");
+  taskEl.classList.add("task__content-el");
+  taskEl.classList.add("blank");
+  taskEl.type = "text";
+  taskEl.value = convertedTask;
+  taskEl.setAttribute("readonly", "readonly");
+
+  taskBtnsContainer = document.createElement("div");
+  taskBtnsContainer.classList.add("task__btns-container");
+
+  taskBtnEdit = document.createElement("button");
+  taskBtnEdit.classList.add("task__btn-edit");
+  taskBtnEdit.textContent = "Edit";
 
   taskBtnDelete = document.createElement("button");
   taskBtnDelete.classList = "task__btn-delete";
   taskBtnDelete.textContent = "Delete";
 
-  taskList.appendChild(taskElContainer);
+  taskList.appendChild(taskContainerEl);
 
-  taskElContainer.appendChild(taskEl);
+  taskContainerEl.appendChild(taskDateElContainer);
+  taskDateElContainer.appendChild(taksDateYearEl);
+  taskDateElContainer.appendChild(taksMonthYearEl);
+  taskDateElContainer.appendChild(taksDayYearEl);
 
-  taskElContainer.appendChild(taskBtnDelete);
+  taskContainerEl.appendChild(taskContentEl);
+  taskContentEl.appendChild(taskEl);
+
+  taskContainerEl.appendChild(taskBtnsContainer);
+  taskBtnsContainer.appendChild(taskBtnEdit);
+  taskBtnsContainer.appendChild(taskBtnDelete);
 
   taskInput.value = "";
+
+  taskBtnEdit.addEventListener("click", () => {
+    if (taskBtnEdit.textContent === "Edit") {
+      taskBtnEdit.textContent = "Save";
+      taskEl.removeAttribute("readonly");
+      taskEl.focus();
+    } else {
+      taskBtnEdit.textContent = "Edit";
+      taskEl.setAttribute("readonly", "readonly");
+    }
+  });
+
+  taskBtnDelete.addEventListener("click", () => {
+    taskList.removeChild(taskContainerEl);
+  });
 };
 
 form.addEventListener("submit", renderTask);
-
-// let toDosArr = [];
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const data = new FormData(form);
-//   data.forEach((toDo) => {
-//     toDosArr.push(toDo);
-//   });
-//   console.log(toDosArr);
-// });
-
-// const renderToDos = (toDos = ["fesfs"]) => {
-//   toDoList.innerHTML = "";
-
-//   toDos.forEach((toDo) => {
-//     const html = `<div>${toDo}</div>`;
-
-//     toDoList.insertAdjacentHTML("afterbegin", html);
-//   });
-// };
-
-// renderToDos();
